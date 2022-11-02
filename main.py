850bf8e72bee21559286347edbd8b8984141990f
吳俊逸 <jim30205@gmail.com>
2022-11-02 14:41:03 +0800

    顯示時間

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+from datetime import datetime
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
+        now =datetime.now()
+        now_str = now.strftime("%Y-%m-%d %H:%M:%S")
+        tk.Label(self,text=now_str,font=("Arial",30)).pack(padx=50,pady=30)
 def main():
     window=Window()
     window.title("數位時鐘")
